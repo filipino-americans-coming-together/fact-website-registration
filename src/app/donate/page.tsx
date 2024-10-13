3d23e6c58c050b655f94d46ca53f92e42e47170f
Alyssa Daniels <asagedaniels@gmail.com>
2024-10-13 12:31:07 -0500

    updated donation description

--- src/app/donate/page.tsx
+++ src/app/donate/page.tsx
@@ -23,20 +23,28 @@ export default function Donate() {
 
             <div className="w-9/12 mx-auto text-center flex flex-col items-center">
                 <h1 className="font-bold sm:text-4xl lg:text-6xl">
-                    Sponsor FACT
+                    Support FACT
                 </h1>
                 <br />
                 <p className="w-8/12">
-                    The Filipino Americans Coming Together (FACT) conference is
-                    an annual conference organized by the Philippine Student
-                    Association at University of Illinois and Urbana-Champaign.
-                    To learn more, visit{" "}
-                    <a className="underline" href="/">
-                        fact.psauiuc.org
-                    </a>
-                    .
+                    The Filipinx Americans Coming Together (FACT) conference is
+                    an annual student-run conference organized by the Philippine
+                    Student Association at University of Illinois at
+                    Urbana-Champaign.
                 </p>
                 <br />
+                <p className="w-8/12">
+                    Your donation to the FACT Conference helps us bring
+                    facilitators, panelists, keynote speakers, headliners,
+                    venues, and much more for our attendees to enjoy and benefit
+                    from. This conference would not be possible without the help
+                    of generous donors like you. But your help goes beyond
+                    sponsorship. By supporting this conference, you contribute
+                    to inspiring and empowering today’s generation to be
+                    passionate and responsible leaders, who aim to shape their
+                    world and follow their dreams. You are laying the groundwork
+                    to strengthen the upcoming generation.
+                </p>
                 <div id="bbox-root"></div>
             </div>
         </>
